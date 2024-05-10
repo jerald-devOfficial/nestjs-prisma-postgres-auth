f64b904010c9094d3334be1dd1befe18b0c569ab
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-05-10 23:02:35 +0800

    Initial Auth routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Post, Get, Controller } from '@nestjs/common'
+import { AuthService } from './auth.service'
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @Post('signup')
+  signUp() {
+    return 'signup route'
+  }
+
+  @Post('signin')
+  signIn() {
+    return 'signin route'
+  }
+
+  @Get('signout')
+  signOut() {
+    return 'signout route'
+  }
 }
