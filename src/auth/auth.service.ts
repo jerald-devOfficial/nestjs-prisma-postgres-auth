2f62a3ca9af9f732bbe9bf996611dd7646fb522e
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-05-16 00:24:28 +0800

    Use PrismaService to auth controller

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,21 +1,25 @@
-import { Injectable } from '@nestjs/common'
+import { Get, Injectable, Post } from '@nestjs/common'
+import { PrismaService } from 'prisma/prisma.service'
 
 @Injectable()
 export class AuthService {
-  constructor() {}
+  constructor(private readonly prisma: PrismaService) {}
 
+  @Post('signup')
   async signUp() {
     return {
       message: 'Successfully signed up!'
     }
   }
 
+  @Post('signin')
   async signIn() {
     return {
       message: 'Sign in was successful!'
     }
   }
 
+  @Get('signout')
   async signOut() {
     return {
       message: 'User has been logged out!'
