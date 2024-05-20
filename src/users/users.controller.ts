1bda7cac879355eb1de7b80744b8aae36fd7284a
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-05-20 23:34:13 +0800

    feat: update getUser and getUsers by correctly not returning the hashedPassword

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,24 @@
-import { Controller } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Controller, Get, Param, Req, UseGuards } from '@nestjs/common'
+import { UsersService } from './users.service'
+import { User } from '@prisma/client'
+import { JwtAuthGuard } from 'src/auth/jwt.guard'
+import { Request } from 'express'
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
+
+  @UseGuards(JwtAuthGuard)
+  @Get(':id')
+  async getUser(
+    @Param() params: { id: string },
+    @Req() req: Request
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.usersService.getUser(params.id, req)
+  }
+
+  @Get()
+  async getUsers(): Promise<Omit<User, 'hashedPassword'>[]> {
+    return this.usersService.getUsers()
+  }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,51 @@
-import { Injectable } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException
+} from '@nestjs/common'
+import { User } from '@prisma/client'
+import { Request } from 'express'
+import { PrismaService } from 'prisma/prisma.service'
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUser(
+    id: string,
+    req: Request
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        id
+      }
+    })
+
+    if (!user) {
+      throw new NotFoundException()
+    }
+
+    const decodedUser = req.user as { id: string; email: string }
+
+    if (user.id !== decodedUser.id) {
+      throw new ForbiddenException()
+    }
+
+    delete user.hashedPassword
+
+    return user
+  }
+
+  async getUsers(): Promise<Omit<User, 'hashedPassword'>[]> {
+    return this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        createdAt: true,
+        updatedAt: true
+      }
+    })
+  }
+}
