f14082804ee06c459e9e83ac53de1e3b0a748287
jerald-devOfficial <76034703+jerald-devOfficial@users.noreply.github.com>
2024-05-20 23:30:51 +0800

    feat: Add name for Signup and create SignInDto

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -12,4 +12,25 @@ export class AuthDto {
     message: 'Password must be between 8 and 20 characters long'
   })
   public password: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(2, 20, {
+    message: 'Name must be between 2 and 20 characters long'
+  })
+  public name: string
+}
+
+export class SignInDto {
+  @IsNotEmpty()
+  @IsString()
+  @IsEmail()
+  public email: string
+
+  @IsNotEmpty()
+  @IsString()
+  @Length(8, 20, {
+    message: 'Password must be between 8 and 20 characters long'
+  })
+  public password: string
 }
